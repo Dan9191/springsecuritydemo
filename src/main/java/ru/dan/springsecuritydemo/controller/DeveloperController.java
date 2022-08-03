d081bffbb3f08629dd19f62afe8403f7e15677e1
Dan <example@email.com>
2022-08-04 01:39:10 +0400

    Конфигурация spring security + авторизация с использованием ролей

---
+++ src/main/java/ru/dan/springsecuritydemo/controller/DeveloperController.java
@@ -0,0 +1,44 @@
+package ru.dan.springsecuritydemo.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.dan.springsecuritydemo.model.Developer;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperController {
+
+    private List<Developer> DEVELOPERS = Stream.of(
+            new Developer(1L, "Ivan", "Ivanov"),
+            new Developer(2L, "Sergey", "Sergeev"),
+
+            new Developer(3L, "Petr", "Petrov")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS.stream()
+                .filter( developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping
+    public Developer create(@RequestBody Developer developer) {
+        this.DEVELOPERS.add(developer);
+        return developer;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletedById(@PathVariable Long id) {
+        this.DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
+    }
+}
