d64f4b554e259a1fe3d1476ec5d63953934951db
Dan <example@email.com>
2022-08-04 01:59:22 +0400

    авторизация с использованием прав доступа (через permission)

---
+++ src/main/java/ru/dan/springsecuritydemo/model/Permission.java
@@ -0,0 +1,15 @@
+package ru.dan.springsecuritydemo.model;
+
+import lombok.Getter;
+
+@Getter
+public enum Permission {
+    DEVELOPERS_READ("developers:read"),
+    DEVELOPERS_WRITE("developers:write");
+
+    private final String permission;
+
+    Permission(String permission) {
+        this.permission = permission;
+    }
+}
