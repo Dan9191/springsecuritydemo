3ae8dce5ee0b698570cdca53da36613359c5bfc1
Dan <example@email.com>
2022-08-04 23:06:36 +0400

    страничка для авторизации

--- src/main/java/ru/dan/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/ru/dan/springsecuritydemo/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ru.dan.springsecuritydemo.model.Role;
 
 @Configuration
@@ -27,7 +28,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/auth/login").permitAll()
+                .defaultSuccessUrl("/auth/success")
+                .and()
+                .logout() //настройка логаута
+                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
+                .invalidateHttpSession(true) //инвалидация сессии
+                .clearAuthentication(true)  //очистка аутентификации
+                .deleteCookies("JSESSIONID") //удаление куки
+                .logoutSuccessUrl("/auth/login");
     }
 
     //автоматизация с использованием ролей admin и user
